583dc6f95266a976bb87a82c6820c45cf6d5d871
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2023-06-22 17:56:41 -0500

    Update db.py

--- SAE_formularios_ms/config/db.py
+++ SAE_formularios_ms/config/db.py
@@ -4,4 +4,4 @@ from pymongo import MongoClient
 
 
 #conn = MongoClient("mongodb://sae_formularios_db:27017/")
-conn = MongoClient("mongodb://mongo_formularios:password@35.195.67.104:27017/SAE_formularios_db")
+conn = MongoClient("mongodb://mongo_formularios:password@10.0.1.20:27017/SAE_formularios_db")
